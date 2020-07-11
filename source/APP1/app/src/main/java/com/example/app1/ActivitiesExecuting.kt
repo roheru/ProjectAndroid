f2ca2336b8fe130a95f492ffa8e63850b9c58e87
roheru <roheru87@gmail.com>
2020-07-11 10:29:56 -0500

    Add activity task
    development activity task module fragment

--- source/APP1/app/src/main/java/com/example/app1/ActivitiesExecuting.kt
+++ source/APP1/app/src/main/java/com/example/app1/ActivitiesExecuting.kt
@@ -1,6 +1,7 @@
 package com.example.app1
 
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,9 @@ import android.view.ViewGroup
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import androidx.fragment.app.Fragment
+import com.example.app1.entities.Activity
 import com.example.app1.entities.Project
+import com.example.app1.models.ModelActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -32,12 +35,17 @@ class ActivitiesExecuting : Fragment(), AdapterView.OnItemSelectedListener  {
     private var param1: String? = null
     private var param2: String? = null
 
-    var states = arrayOf("-Seleccione una opción-", "No finalizada","Finalizada" )
-    var spinnerp: Spinner? = null
-    var spinners: Spinner? = null
-    var tv: TextView?=null
-    var viz:Button?=null
-
+    private var states = arrayOf("-Seleccione una opción-", "No finalizada","Finalizada" )
+    private var spinnerp: Spinner? = null
+    private var spinners: Spinner? = null
+    private var na:EditText?=null
+    private var rt:EditText?=null
+    private var da:EditText?=null
+    private var tv: TextView?=null
+    private var msg: TextView?=null
+    private var viz:Button?=null
+    private var save:Button?=null
+    private var idProject:String?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,29 +56,38 @@ class ActivitiesExecuting : Fragment(), AdapterView.OnItemSelectedListener  {
         accionar(viewv)
         return viewv
     }
+
+
     fun initializeComponents(v:View){
 
         val projects: ArrayList<Project> =getProjects()
         var  db= FirebaseAuth.getInstance()
+
         v?.welcome?.text="Buen Día "+db.currentUser?.email.toString()
-        tv=v.description
-        viz=v.visualize
-        spinnerp =v.projects_works
-        spinners=v.states_spinner
+        this.tv=v.description
+        this.na=v.nameActivityText
+        this.rt=v.responsableText
+        this.da=v.descriptionText
+        this.viz=v.visualize
+        this.save=v.save
+        this.msg=v.msg_user
+        this.spinnerp =v.projects_works
+        this.spinners=v.states_spinner
+
         val projectsList = ArrayAdapter(this.requireContext(), android.R.layout.simple_spinner_item, projects)
         projectsList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerp!!.adapter = projectsList
+        this.spinnerp!!.adapter = projectsList
 
         val stateList = ArrayAdapter(this.requireContext(), android.R.layout.simple_spinner_item, states)
         stateList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinners!!.adapter = stateList
+        this.spinners!!.adapter = stateList
 
     }
 
 
     fun getProjects():ArrayList<Project>{
         val projects: ArrayList<Project> = ArrayList()
-        projects.add( Project("-Seleccione un proyecto",""," "))
+        projects.add( Project(" - Seleccione un proyecto - ",""," "))
         //projects.add()
         try {
 
@@ -88,7 +105,7 @@ class ActivitiesExecuting : Fragment(), AdapterView.OnItemSelectedListener  {
                         for (document in task.result!!) {
                             Log.d("DataValueID", document.id + " => " + document.data.get("name"))
                             //this.projects.set(this.projects.size,)
-                            projects.add( Project(document.data.get("name").toString(),document.data.get("description").toString()," "))
+                            projects.add( Project(document.data.get("name").toString(),document.data.get("description").toString(),document.id))
                         }
                     } else {
                         Log.d("DataValueError", "Error getting documents: ", task.exception)
@@ -103,39 +120,103 @@ class ActivitiesExecuting : Fragment(), AdapterView.OnItemSelectedListener  {
 
     }
 
+    fun saveActivity() {
+        Log.i("activityValue",this.spinners?.selectedItemPosition.toString())
+        var numberIdProject:Int?=this.spinnerp?.selectedItemPosition?.toInt()
+        var numberStateValue:Int?=this.spinners?.selectedItemPosition?.toInt()
+        if(this.spinnerp!=null && numberIdProject!=null && numberIdProject>0){
+            if (this.na != null && !this.na?.text.toString().isEmpty()) {
+                if(this.rt!=null && !this.rt?.text.toString().isEmpty()){
+                    if(this.da!=null && !this.da?.text.toString().isEmpty()) {
+                        if(numberStateValue!=null && numberStateValue>0){
+                            var act:Activity?=null
+                            if(numberStateValue==1){
+                                act=Activity(this.na?.text.toString(),this.rt?.text.toString(),this.da?.text.toString(), false, this.idProject.toString())
+                            }else{
+                                act=Activity(this.na?.text.toString(),this.rt?.text.toString(),this.da?.text.toString(), true, this.idProject.toString())
+                            }
+                            var ma:ModelActivity?=ModelActivity()
+                            var r:Boolean?=ma?.insertActivity(act)
+
+                            if(r==true){
+                                this.na?.text=Editable.Factory.getInstance().newEditable("")
+                                this.rt?.text=Editable.Factory.getInstance().newEditable("")
+                                this.da?.text=Editable.Factory.getInstance().newEditable("")
+                                //this.spinnerp.setSelection(0)
+                                this.spinnerp?.setSelection(0)
+                                this.spinners?.setSelection(0)
+                                this.viz?.isEnabled=false
+                                this.save?.isEnabled=false
+                                this.msg?.text="Actividad Creada con Éxito!!"
+
+                            }else{
+                                this.msg?.text="La actividad no pudo ser creada"
+                            }
+
+
+                        }else{
+                            this.msg?.text = "Por favor seleccione el estado de la actividad"
+                        }
+                    }else{
+                        this.msg?.text = "Por favor digite la descripción de la actividad"
+
+                    }
+                }else{
+                    this.msg?.text = "Por favor digite el responsable de la actividad"
+
+                }
+            } else {
+                this.msg?.text = "Por favor digite un nombre de la actividad"
+            }
+        }else{
+            this.msg?.text = "Por favor seleccione un proyecto"
+        }
+
+
+
+    }
+
+    fun itemSelection(position:Int,v: AdapterView<*>?){
+        Log.i("positionProj",position.toString())
+        if(position>0){
+            val s=v?.getItemAtPosition(position) as Project
+            Log.i("descriptionProj",s.toString())
+            this.viz?.isEnabled=true
+            this.save?.isEnabled=true
+            this.tv?.text=s.getId().toString()+"-"+s.getDescription()
+            this.idProject=s.getId().toString()
+        }else{
+            this.viz?.isEnabled=false
+            this.save?.isEnabled=false
+            this.tv?.text=""
+        }
+
+    }
 
     fun accionar(v:View){
         this.spinnerp?.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onItemSelected(
-                parentView: AdapterView<*>?,
-                selectedItemView: View,
-                position: Int,
-                id: Long
-            ) {
+            override fun onItemSelected(parentView: AdapterView<*>?,selectedItemView: View,position: Int,id: Long) {
                 try {
-
-                    if(position>0){
-                        val s=parentView?.getItemAtPosition(position) as Project
-                        Log.i("descriptionProj",s.toString())
-                        viz?.isEnabled=true
-                        tv?.text=s.getId().toString()+"-"+s.getDescription()
-                    }else{
-                        viz?.isEnabled=false
-                        tv?.text=""
-                    }
-
+                    itemSelection(position,parentView)
                 }catch (e:Exception){
                     e.printStackTrace()
-
                 }
-
-
             }
 
             override fun onNothingSelected(parentView: AdapterView<*>?) {
                 // your code here
             }
         }
+
+        this.save?.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                saveActivity()
+                //var state=
+
+            }
+        })
+
+
     }
 
     companion object {
---
+++ source/APP1/app/src/main/java/com/example/app1/models/ModelActivity.kt
@@ -0,0 +1,39 @@
+package com.example.app1.models
+
+import android.util.Log
+import com.example.app1.entities.Activity
+import com.google.firebase.firestore.FirebaseFirestore
+import java.lang.Exception
+
+class ModelActivity {
+
+
+
+
+    constructor(){
+
+    }
+
+    fun insertActivity(activity:Activity):Boolean{
+        var flag=false
+        try {
+            var db = FirebaseFirestore.getInstance()
+            val dbActivity = db.collection("activities")
+            val activ=activity.toMap()
+
+            dbActivity.add(activ).addOnSuccessListener { documentReference ->
+                flag=true
+
+            }.addOnFailureListener { e ->
+                flag=false
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+
+        }
+        Log.i("flag",flag.toString())
+        return true
+
+    }
+
+}
